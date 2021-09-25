627e81c84c490c3b251c98cd07e911c0c67a8fb0
bottassiJuanIgnacio <bottassi2012@gmail.com>
2021-09-25 16:32:27 -0300

    Hasta clase 14 Flexbox

--- intro-html-vivo/app.js
+++ intro-html-vivo/app.js
@@ -13,3 +13,16 @@ app.listen(3000, () =>{
 app.get('/', (req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
 })
+
+app.get('/register', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+})
+app.post('/register', (req,res)=>{
+    res.send("Felicitaciones, tu registro a sido exitoso !")
+})
+app.get('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
+app.post('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
+})
